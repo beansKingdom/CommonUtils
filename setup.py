10dc173a8fb602e72d3ac29740f89f59fac8614d
hly <helingyun02@meituan.com>
2019-09-17 15:03:28 +0800

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BeanCommonUtils",
-    version="0.1",
+    version="0.1.1",
     author="Hly",
     author_email="hlyaction@gmail.com",
     description="A Python library for common methods.",
