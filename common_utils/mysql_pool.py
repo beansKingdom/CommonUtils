8f3edbcbcc900ba000c14dc6b00e27f7851939ec
hly <764676555@qq.com>
2019-10-04 21:37:59 +0800

    fix mysql pool

--- common_utils/mysql_pool.py
+++ common_utils/mysql_pool.py
@@ -4,55 +4,60 @@ from DBUtils.PooledDB import PooledDB
 
 class MysqlPool(object):
 
-    __pool = None
-
-    def __init__(self, mysql_info):
-        # 构造函数，创建数据库连接、游标
-        self.mysql_conn = MysqlPool.get_mysql_conn(mysql_info)
-        self.cur = self.mysql_conn.cursor(cursor=pymysql.cursors.DictCursor)
+    pool = None
 
     # 数据库连接池连接
-    @classmethod
-    def get_mysql_conn(cls, mysql_info):
-        if cls.__pool is None:
-            cls.__pool = PooledDB(creator=pymysql, mincached=5, maxcached=20,
-                                  host=mysql_info['host'], user=mysql_info['user'], passwd=mysql_info['passwd'],
-                                  db=mysql_info['db'], port=mysql_info['port'],
-                                  charset=mysql_info.get('charset', 'utf8'))
+    def init_mysql_pool(self, mysql_info):
+        if self.pool is None:
+            self.pool = PooledDB(creator=pymysql, mincached=10,
+                                 host=mysql_info['host'], user=mysql_info['user'], passwd=mysql_info['passwd'],
+                                 db=mysql_info['db'], port=mysql_info['port'],
+                                 maxcached=20,  # 链接池中最多闲置的链接，0和None不限制
+                                 blocking=True,
+                                 ping=0,
+                                 charset=mysql_info.get('charset', 'utf8'),
+                                 maxconnections=6)
 
-        return cls.__pool.connection()
+    def get_mysql_conn(self):
+        mysql_conn = self.pool.connection()
+        cur = mysql_conn.cursor(cursor=pymysql.cursors.DictCursor)
+        return cur, mysql_conn
 
     # 插入\更新\删除sql
-    def op_insert(self, sql, sql_type):
+    @staticmethod
+    def op_insert(sql, cur, mysql_conn, sql_type):
+        mysql_conn.ping()
         try:
-            insert_num = self.cur.execute(sql)
-            self.mysql_conn.commit()
+            insert_num = cur.execute(sql)
+            mysql_conn.commit()
         except Exception as e:
             raise Exception("%s sql execute error, err_msg: %s" % (sql_type, e))
 
         return insert_num, True
 
     # 查询
-    def op_select(self, sql):
-        self.cur.execute(sql)
+    @staticmethod
+    def op_select(sql, cur, mysql_conn):
+        mysql_conn.ping()
+        cur.execute(sql)
         try:
-            select_res = self.cur.fetchall()
+            select_res = cur.fetchall()
         except Exception as e:
             return e, False
 
         return select_res, True
 
-    def sql_operate(self, sql, sql_type):
+    def sql_operate(self, sql, cur, mysql_conn, sql_type):
         sql_operate_list = ["insert", "update", "delete", "select"]
         if not isinstance(sql_type, str) and sql_type not in sql_operate_list:
             raise ValueError("input sql_type error, sql_type may be: %s" % sql_operate_list)
 
         if sql_type == "select":
-            return self.op_select(sql)
+            return self.op_select(sql, cur, mysql_conn)
         else:
-            return self.op_insert(sql, sql_type)
+            return self.op_insert(sql, cur, mysql_conn, sql_type)
 
     # 释放资源
-    def dispose(self):
-        self.mysql_conn.close()
-        self.cur.close()
+    @staticmethod
+    def dispose(mysql_conn):
+        mysql_conn.close()
